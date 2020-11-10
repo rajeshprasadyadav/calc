a19451244cf9464f22ed710d7e24ee7686d416be
Rajesh Prasad Yadav <rajeshy@ispace.com>
2020-11-10 20:05:02 +0530

    added comment

--- calculator.js
+++ calculator.js
@@ -1,8 +1,11 @@
 const sum = (a,b)=>a+b;
 const sub = (a,b)=>a-b;
 const mul = (a,b)=>a*b;
+//added remindr
+const reminder = (a,b)=>a%b;
 module.exports={
     sum,
     sub,
-    mul
+    mul,
+    reminder
 }
