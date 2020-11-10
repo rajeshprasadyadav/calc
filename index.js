783ce5c41ddec1cc31b3c3b0254caac74c1a509f
Rajesh Prasad Yadav <rajeshy@ispace.com>
2020-11-10 20:00:07 +0530

    added reminder functionalities

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { sum , sub, mul} = require('./calculator')
+const { sum , sub, mul,reminder} = require('./calculator')
 console.log(sum(2,3));
 console.log(sub(5,2));
 console.log(mul(2,3));
-
+console.log(reminder(5,2))
